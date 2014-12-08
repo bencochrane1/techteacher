673aafa6f7f6ed8eeb5f2faba31d1d72c2c23370
Ben Cochrane <bencochrane1@me.com>
2014-12-08 21:56:54 +1100

    add in new and show pages for videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,7 @@
 class VideosController < ApplicationController
-  
+  before_action :set_video, only: [:show]
+
+
   def index
     @videos = Video.all
   end
@@ -12,18 +14,34 @@ class VideosController < ApplicationController
     @video = Video.find params[:id]
   end
 
+  def create
+    @video = Video.new video_params
+
+    if @video.save
+        redirect_to @video
+    else
+        render :new
+    end
+
+  end
+
   def show
     @video = Video.find params[:id]
   end
 
-  private 
-  def restaurant_params
-      params.require(:video).permit(:title, 
-                                    :description, 
-                                    :category,
-                                    :author_url,
-                                    :author, 
-                                    :youtube_url)
-    end
+  def new
+    @video = Video.new
+  end
+
+
+  private
+
+  def video_params
+    params.require(:video).permit(:title, :description, :category, :author, :author_url, :youtube_url)
+  end
+
+  def set_video
+    @video = Video.find(params[:id])
+  end
 
 end
