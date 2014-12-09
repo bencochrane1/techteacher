c42af1a6f6234d226cbe71002dfbd14155fde5a1
Ben Cochrane <bencochrane1@me.com>
2014-12-09 17:00:52 +1100

    improve category_id in schema

--- db/migrate/20141208230829_change_category_to_category_id_in_videos.rb
+++
@@ -1,6 +0,0 @@
-class ChangeCategoryToCategoryIdInVideos < ActiveRecord::Migration
-  def change
-    rename_column :videos, :category, :category_id
-    change_column :videos, :category_id, :integer
-  end
-end
--- db/migrate/20141208231337_change_limit_of_category_id_in_videos.rb
+++
@@ -1,5 +0,0 @@
-class ChangeLimitOfCategoryIdInVideos < ActiveRecord::Migration
-  def change
-    change_column :videos, :category_id, :integer, :limit => nil
-  end
-end
