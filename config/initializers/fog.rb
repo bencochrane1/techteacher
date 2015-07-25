99ca8df14473a9ed2014319b16caaa6ae8721e03
Ben Cochrane <bencochrane1@me.com>
2015-07-25 12:26:41 +1000

    Update fog.rb

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAI4LRMHYPFY4UAU4A',                        # required
-    :aws_secret_access_key  => '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',                        # required
+    :aws_access_key_id      => '',                        # required
+    :aws_secret_access_key  => '',                        # required
     :region => "ap-southeast-1"
   }
   config.fog_directory  = 'techteacher'                          # required
